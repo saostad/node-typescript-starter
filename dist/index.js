760cc921fcde57dc1b2ff760c6eb5a688d6defac
Ostad <SOstad@kajimausa.com>
2020-05-18 15:33:33 -0700

    package: updated

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const fast_node_logger_1 = require("fast-node-logger");
