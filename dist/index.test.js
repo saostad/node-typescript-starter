3b311cb33f533129d8637e31a0b00d8a77726889
Saeid Ostad <sostad@kajimausa.com>
2021-01-14 22:04:41 -0500

    feat: added --trace-warning in dev script removed extra files

--- dist/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-test("process.env", async () => {
-    const result = await index_1.main();
-    expect(result).toBe("Hellooooo");
-});
-//# sourceMappingURL=index.test.js.map
--- dist/typings/node/mode.d.ts
+++
@@ -1,1 +0,0 @@
-export declare type NodeMode = "development" | "production";
